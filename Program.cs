686c5554370d40ef133e5e34bc237bf23ab23e4b
Julian <julian.ortmann@gmx.de>
2021-05-19 11:58:53 +0200

    aufgabe sechs

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Schule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entfernung zum Zielort: ");
+            int km = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Alter des Reisenden: ");
+            int alter = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Einfache Fahrt? j/n: ");
+            string einfach = Console.ReadLine();
+
+            double preis = km * 0.2;
+
+            if (alter < 14)
+            {
+                if (einfach == "j")
+                {
+                    preis = preis / 2;
+                }
+                else
+                {
+                    preis = preis * 0.6;
+                }
+            }
+            if(alter > 65){
+                if (einfach == "j")
+                {
+                    preis = preis * 0.7;
+                }
+                else
+                {
+                    preis = preis * 0.6;
+                }
+            }
+            else
+            {
+                if (einfach == "n")
+                {
+                    preis = preis * 0.8;
+                }
+            }
+
+            Console.Write("Der Preis beträgt: {0} Euro", preis);
+
+        }
+    }
+}
