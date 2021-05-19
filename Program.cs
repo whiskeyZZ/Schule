73d1b6a65e768c71feb0bd52a91a89a2d3bba0f4
Julian <julian.ortmann@gmx.de>
2021-05-19 12:21:41 +0200

    Aufgabe Sieben

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ namespace Schule
     {
         static void Main(string[] args)
         {
+            //AufgabeSechs();
+            //Stromkosten();
+            Tarifoptimierer();
+        }
+
+        static void AufgabeSechs()
+        {
             Console.Write("Entfernung zum Zielort: ");
             int km = Convert.ToInt32(Console.ReadLine());
             Console.Write("Alter des Reisenden: ");
@@ -26,7 +33,8 @@ namespace Schule
                     preis = preis * 0.6;
                 }
             }
-            if(alter > 65){
+            if (alter > 65)
+            {
                 if (einfach == "j")
                 {
                     preis = preis * 0.7;
@@ -45,6 +53,43 @@ namespace Schule
             }
 
             Console.Write("Der Preis beträgt: {0} Euro", preis);
+        }
+
+        static void Stromkosten()
+        {
+            string tarif = "1";
+            double verbrauch = 1000;
+            double preis = 0;
+
+            if (tarif == "1")
+            {
+                preis = verbrauch * 0.12 + 80;
+            }
+            if (tarif == "2")
+            {
+                preis = verbrauch * 0.15 + 50;
+            }
+
+            Console.WriteLine("Der Preis beträgt: {0} Euro", preis);
+        }
+
+        static void Tarifoptimierer(){
+            double preiseins = 0;
+            double preiszwei = 0;
+            double verbrauch = 8000;
+
+            preiseins = verbrauch * 0.12 + 80;
+            preiszwei = verbrauch * 0.15 + 50;
+
+            if (preiseins > preiszwei){
+                Console.Write("Tarif 2 ist der beste");
+            }
+            else if (preiseins < preiszwei){
+                Console.Write("Tarif 1 ist der beste");
+            }
+            else {
+                Console.Write("Beide Tarife sind gleich gut");
+            }
 
         }
     }
